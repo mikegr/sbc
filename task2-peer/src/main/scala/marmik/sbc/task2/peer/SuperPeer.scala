a1fe91b80c530ce3b8e4e9ba48d8a218652dfefb
Michael Greifeneder <mike@aphrodite.(none)>
2008-12-07 11:51:47 +0100

    merge

--- task2-peer/src/main/scala/marmik/sbc/task2/peer/SuperPeer.scala
+++ task2-peer/src/main/scala/marmik/sbc/task2/peer/SuperPeer.scala
@@ -2,11 +2,21 @@ package marmik.sbc.task2.peer
 
 trait SuperPeer {
   
-  def login(url: String, selfName: String, selfUrl: String);
+  /** 
+   * @param url: Url of super pper
+   * @param selfName: Name of this peer
+   * @param url: Url of this peer 
+   */
+  def login(url:String, selfName:String, selfUrl:String);
 
-  def registerListener(l: Listener);
+  /**
+   Registers a listener to get notification about new posts or edits.
+   */
+  def register(l:Listener);
 
-  def peers(): List[Peer];
+  /** Returns list of available peers
+   */
+  def getPeers():List[Peer];
   
   def logout();  
   
