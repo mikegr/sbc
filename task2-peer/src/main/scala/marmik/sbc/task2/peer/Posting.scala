37f806a05b19cab03561ded8503bb7720113340d
Martin Schürrer <martin@schuerrer.org>
2008-12-07 03:05:31 +0100

    Changed Date to GregorianCalendar since Date is mostly deprecated

--- task2-peer/src/main/scala/marmik/sbc/task2/peer/Posting.scala
+++ task2-peer/src/main/scala/marmik/sbc/task2/peer/Posting.scala
@@ -1,12 +1,12 @@
 package marmik.sbc.task2.peer
 
-import java.util.Date
+import java.util.GregorianCalendar
 
 trait Posting {
 
   def author(): String;
   def subject(): String;
-  def createdAt(): Date;
+  def createdAt(): GregorianCalendar;
   def content(): String;
   
   def replies(): List[Posting];
