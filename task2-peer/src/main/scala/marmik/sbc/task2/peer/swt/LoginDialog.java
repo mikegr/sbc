8bdabfeebcbcbd83393863cc297555690fdd522f
Martin Schürrer <martin@schuerrer.org>
2008-12-12 16:48:17 +0100

    Reformatted LoginDialog

--- task2-peer/src/main/scala/marmik/sbc/task2/peer/swt/LoginDialog.java
+++ task2-peer/src/main/scala/marmik/sbc/task2/peer/swt/LoginDialog.java
@@ -19,91 +19,95 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
 public class LoginDialog extends Dialog {
-	private DataBindingContext m_bindingContext;
-	private Binding testBinding;
-	private Text peerNameText;
-	private Combo urlCombo;
-	private Combo serviceCombo;
-	/**
-	 * Create the dialog
-	 * @param parentShell
-	 */
-	public LoginDialog(Shell parentShell) {
-		super(parentShell);
-	}
+  private DataBindingContext m_bindingContext;
+  private Binding testBinding;
+  private Text peerNameText;
+  private Combo urlCombo;
+  private Combo serviceCombo;
 
-	/**
-	 * Create contents of the dialog
-	 * @param parent
-	 */
-	@Override
-	protected Control createDialogArea(Composite parent) {
-		Composite container = (Composite) super.createDialogArea(parent);
-		final GridLayout gridLayout = new GridLayout();
-		gridLayout.numColumns = 2;
-		container.setLayout(gridLayout);
+  /**
+   * Create the dialog
+   * 
+   * @param parentShell
+   */
+  public LoginDialog(Shell parentShell) {
+    super(parentShell);
+  }
 
-		final Label messageLabel = new Label(container, SWT.NONE);
-		final GridData gd_messageLabel = new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1);
-		gd_messageLabel.minimumWidth = 350;
-		messageLabel.setLayoutData(gd_messageLabel);
-		messageLabel.setText("Bitte melden Sie sich an.");
+  /**
+   * Create contents of the dialog
+   * 
+   * @param parent
+   */
+  @Override
+  protected Control createDialogArea(Composite parent) {
+    Composite container = (Composite) super.createDialogArea(parent);
+    final GridLayout gridLayout = new GridLayout();
+    gridLayout.numColumns = 2;
+    container.setLayout(gridLayout);
 
-		final Label nameLabel = new Label(container, SWT.NONE);
-		nameLabel.setLayoutData(new GridData());
-		nameLabel.setText("Name:");
+    final Label messageLabel = new Label(container, SWT.NONE);
+    final GridData gd_messageLabel = new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1);
+    gd_messageLabel.minimumWidth = 350;
+    messageLabel.setLayoutData(gd_messageLabel);
+    messageLabel.setText("Bitte melden Sie sich an.");
 
-		peerNameText = new Text(container, SWT.BORDER);
-		final GridData gd_peerNameText = new GridData(SWT.FILL, SWT.CENTER, true, false);
-		peerNameText.setLayoutData(gd_peerNameText);
+    final Label nameLabel = new Label(container, SWT.NONE);
+    nameLabel.setLayoutData(new GridData());
+    nameLabel.setText("Name:");
 
-		final Label urlLabel = new Label(container, SWT.NONE);
-		urlLabel.setText("URL:");
+    peerNameText = new Text(container, SWT.BORDER);
+    final GridData gd_peerNameText = new GridData(SWT.FILL, SWT.CENTER, true, false);
+    peerNameText.setLayoutData(gd_peerNameText);
 
-		final ComboViewer urlComboViewer = new ComboViewer(container, SWT.BORDER);
-		urlCombo = urlComboViewer.getCombo();
-		final GridData gd_urlCombo = new GridData(SWT.FILL, SWT.CENTER, true, false);
-		urlCombo.setLayoutData(gd_urlCombo);
+    final Label urlLabel = new Label(container, SWT.NONE);
+    urlLabel.setText("URL:");
 
-		final Label serviceLabel = new Label(container, SWT.NONE);
-		serviceLabel.setText("Service:");
+    final ComboViewer urlComboViewer = new ComboViewer(container, SWT.BORDER);
+    urlCombo = urlComboViewer.getCombo();
+    final GridData gd_urlCombo = new GridData(SWT.FILL, SWT.CENTER, true, false);
+    urlCombo.setLayoutData(gd_urlCombo);
 
-		final ComboViewer serviceComboViewer = new ComboViewer(container, SWT.READ_ONLY);
-		serviceCombo = serviceComboViewer.getCombo();
-		serviceCombo.setItems(new String[] { "XVSM", "Socket"});
-		final GridData gd_serviceCombo = new GridData(SWT.FILL, SWT.CENTER, true, false);
-		serviceCombo.setLayoutData(gd_serviceCombo);
-		//
-		return container;
-	}
+    final Label serviceLabel = new Label(container, SWT.NONE);
+    serviceLabel.setText("Service:");
 
-	/**
-	 * Create contents of the button bar
-	 * @param parent
-	 */
-	@Override
-	protected void createButtonsForButtonBar(Composite parent) {
-		createButton(parent, IDialogConstants.OK_ID, "Anmelden",
-				true);
-		createButton(parent, IDialogConstants.CANCEL_ID,
-				"Beenden", false);
-		m_bindingContext = initDataBindings();
-	}
+    final ComboViewer serviceComboViewer = new ComboViewer(container, SWT.READ_ONLY);
+    serviceCombo = serviceComboViewer.getCombo();
+    serviceCombo.setItems(new String[] { "XVSM", "Socket" });
+    final GridData gd_serviceCombo = new GridData(SWT.FILL, SWT.CENTER, true, false);
+    serviceCombo.setLayoutData(gd_serviceCombo);
+    //
+    return container;
+  }
 
-	protected void configureShell(Shell newShell) {
-		super.configureShell(newShell);
-		newShell.setText("Anmelden");
-	}
-	protected DataBindingContext initDataBindings() {
-		IObservableValue peerNameTextTextObserveWidget = SWTObservables.observeText(peerNameText, SWT.Modify);
-		IObservableValue urlComboTextObserveValue = BeansObservables.observeValue(urlCombo, "text");
-		//
-		//
-		DataBindingContext bindingContext = new DataBindingContext();
-		//
-		testBinding = bindingContext.bindValue(peerNameTextTextObserveWidget, urlComboTextObserveValue, null, null);
-		//
-		return bindingContext;
-	}
+  /**
+   * Create contents of the button bar
+   * 
+   * @param parent
+   */
+  @Override
+  protected void createButtonsForButtonBar(Composite parent) {
+    createButton(parent, IDialogConstants.OK_ID, "Anmelden", true);
+    createButton(parent, IDialogConstants.CANCEL_ID, "Beenden", false);
+    m_bindingContext = initDataBindings();
+  }
+
+  @Override
+  protected void configureShell(Shell newShell) {
+    super.configureShell(newShell);
+    newShell.setText("Anmelden");
+  }
+
+  protected DataBindingContext initDataBindings() {
+    IObservableValue peerNameTextTextObserveWidget = SWTObservables.observeText(peerNameText, SWT.Modify);
+    IObservableValue urlComboTextObserveValue = BeansObservables.observeValue(urlCombo, "text");
+    //
+    //
+    DataBindingContext bindingContext = new DataBindingContext();
+    //
+    testBinding = bindingContext.bindValue(peerNameTextTextObserveWidget, urlComboTextObserveValue, null, null);
+    //
+    return bindingContext;
+  }
 
 }
