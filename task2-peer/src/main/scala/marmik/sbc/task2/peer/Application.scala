5ff36fa96cca02721d64f5b224ca14b2f5d07584
Martin Schürrer <martin@schuerrer.org>
2008-12-13 00:55:50 +0100

    Simplified LoginDialog invokation

--- task2-peer/src/main/scala/marmik/sbc/task2/peer/Application.scala
+++ task2-peer/src/main/scala/marmik/sbc/task2/peer/Application.scala
@@ -1,9 +1,5 @@
 package marmik.sbc.task2.peer
 
-import org.eclipse.swt.SWT
-import org.eclipse.swt.widgets.Display
-import org.eclipse.swt.widgets.Shell
-
 import marmik.sbc.task2.peer.swt.LoginDialog
 
 object Application {
@@ -11,10 +7,8 @@ object Application {
 
   def main(args: Array[String]): Unit = {
     log.info("Starting Task2 Peer")
-    
-    val display = Display.getDefault
-    val shell = new Shell(display)
-    val loginDialog = new LoginDialog(shell)
+
+    val loginDialog = new LoginDialog(null)
 
     loginDialog.setBlockOnOpen(true)
     loginDialog.open
