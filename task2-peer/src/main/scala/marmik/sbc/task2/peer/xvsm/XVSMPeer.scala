445269bdfd45a4eb3bc685d7d77194385c8768cf
Martin Schürrer <martin@schuerrer.org>
2008-12-19 18:08:52 +0100

    Changed boolean to Boolean & whitespace fixes

--- task2-peer/src/main/scala/marmik/sbc/task2/peer/xvsm/XVSMPeer.scala
+++ task2-peer/src/main/scala/marmik/sbc/task2/peer/xvsm/XVSMPeer.scala
@@ -3,18 +3,18 @@ package marmik.sbc.task2.peer.xvsm
 import marmik.sbc.task2.peer._
 
 class XVSMPeer(ec:EasyCapi, val url:String, val name:String) extends Peer {
-  
+
   def newTopic(name:String):Topic  = {
     throw new UnsupportedOperationException("Not allowed on foreign peer");
   }
-  
+
   def topics():List[Topic] = {
     ec.topics(url);
   }
-  
-  override def equals(o:Any):boolean = {
-    if (! o.isInstanceOf[XVSMPeer] ) false 
-    else if (o.asInstanceOf[XVSMPeer].url != this.url) false 
+
+  override def equals(o:Any):Boolean = {
+    if (! o.isInstanceOf[XVSMPeer] ) false
+    else if (o.asInstanceOf[XVSMPeer].url != this.url) false
     else true;
-  } 
+  }
 }
