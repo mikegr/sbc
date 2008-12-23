85fb820720ff17c0004aa76abdf406d48fc778e7
Michael Greifeneder <mikegr@gmx.net>
2008-12-24 00:28:12 +0100

    Add System Architecture

--- task2-peer/src/main/scala/marmik/sbc/task2/rmi/RemotePeer.scala
+++ task2-peer/src/main/scala/marmik/sbc/task2/rmi/RemotePeer.scala
@@ -15,7 +15,7 @@ trait RemotePeer extends java.rmi.Remote {
   def getReplys(id:Integer):List[PostingInfo];
 
   @throws (classOf[java.rmi.RemoteException])
-  def post(topic:String, id:Integer, author:String, subject:String, content:String ):Integer;
+  def post(topic:String, parent:Integer, author:String, subject:String, content:String ):Integer;
 
   @throws (classOf[java.rmi.RemoteException])
   def edit(id:Integer, content:String);
