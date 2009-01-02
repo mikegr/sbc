3eebff4ad2297729caf864dad4632771f7b1a67a
Martin Schürrer <martin@schuerrer.org>
2009-01-02 23:24:22 +0100

    Fixed imports in XVSMSession

--- task2-peer/src/main/scala/marmik/sbc/task2/peer/xvsm/XVSMSession.scala
+++ task2-peer/src/main/scala/marmik/sbc/task2/peer/xvsm/XVSMSession.scala
@@ -1,5 +1,7 @@
 package marmik.sbc.task2.peer.xvsm
 
+import scala.collection.mutable.ListBuffer
+
 import org.xvsm.interfaces._
 import org.xvsm.core._
 import org.xvsm.transactions._
@@ -10,23 +12,23 @@ import marmik.sbc.task2.peer.xvsm.XVSMContants._
 import marmik.sbc.task2.peer._
 
 class XVSMSession(capi:EasyCapi, superpeer:String, selfUrl:String, selfName:String) extends Session {
-  
+
   val localPeer = new XVSMLocalPeer(capi, selfUrl, selfName); //setting url to null, because of embedded space
-  
-  val listener:ListBuffer = new ListBuffer[Listener]();
-    
+
+  val listener:ListBuffer[Listener] = new ListBuffer[Listener]();
+
   def registerListener(l:Listener) = {
     listener + l;
   }
   def peers():List[Peer] = {
     capi.readPeerInfo();
   }
-    
+
   def logout() {
     capi.logout;
   }
   def dumpTopics() {
      capi.dumpTopics();
-  } 
-  
+  }
+
 }
