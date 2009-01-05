62b8a04125a8ac36b673ff411c54e2aec6c8dde1
Martin Schürrer <martin@schuerrer.org>
2009-01-05 02:44:03 +0100

    Removed obsolete commented code

--- task2-peer/src/main/scala/marmik/sbc/task2/peer/swt/model/Session.java
+++ task2-peer/src/main/scala/marmik/sbc/task2/peer/swt/model/Session.java
@@ -3,8 +3,6 @@ package marmik.sbc.task2.peer.swt.model;
 import java.util.List;
 import org.eclipse.core.databinding.observable.list.WritableList;
 
-import marmik.sbc.task2.peer.Listener;
-
 public class Session extends ModelObject {
   private marmik.sbc.task2.peer.Session backing;
   private WritableList peers;
@@ -13,19 +11,6 @@ public class Session extends ModelObject {
   public Session(marmik.sbc.task2.peer.Session backing, WritableList peers, Peer localPeer) {
     this.backing = backing;
     this.localPeer = localPeer;
-//    backing.registerListener(new Listener() {
-//      public void peerJoins(marmik.sbc.task2.peer.Peer peer) {
-//      }
-//
-//      public void peerLeaves(marmik.sbc.task2.peer.Peer peer) {
-//      }
-//
-//      public void postingCreated(marmik.sbc.task2.peer.Posting posting) {
-//      }
-//
-//      public void postingEdited(marmik.sbc.task2.peer.Posting posting) {
-//      }
-//    });
   }
 
   public marmik.sbc.task2.peer.Session getBacking() {
