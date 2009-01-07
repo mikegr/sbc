a016e812ce0b9aafeacbe5538e69f2f03e17433e
Martin Schürrer <martin@schuerrer.org>
2009-01-07 04:15:37 +0100

    Major bugfixing

--- task2-peer/src/main/scala/marmik/xvsm/Container.scala
+++ task2-peer/src/main/scala/marmik/xvsm/Container.scala
@@ -1,5 +1,6 @@
 package marmik.xvsm
 
+import java.io.Serializable
 import scala.collection.mutable.ListBuffer
 import scalaz.javas.{List => ListZ}
 import marmik.xvsm.Conversions._
@@ -8,22 +9,21 @@ object Container {
   def toXVSMEntry(entry: Any): org.xvsm.core.Entry = {
     entry match {
       case None => new org.xvsm.core.VoidEntry()
-      case Some(x) => toXVSMEntry(x)
+      case Some(x: Serializable) => toXVSMEntry(x)
       case xs@Seq(_*) => toXVSMEntry(ListZ.ScalaList_JavaList(xs.toList))
-      case (a, b) => new org.xvsm.core.Tuple(toXVSMEntry(a, b))
-      case (a, b, c) => new org.xvsm.core.Tuple(toXVSMEntry(a, b, c))
-      case (a, b, c, d) => new org.xvsm.core.Tuple(toXVSMEntry(a, b, c, d))
-      case x => GenericHelper.createAtomicEntry(x)
-    //case x => new org.xvsm.core.AtomicEntry[_](x, x.getClass.asInstanceOf[Class[_]])
+      case (a: Serializable, b: Serializable) => new org.xvsm.core.Tuple(toXVSMEntry(a), toXVSMEntry(b))
+      case (a: Serializable, b: Serializable, c: Serializable) => new org.xvsm.core.Tuple(toXVSMEntry(a), toXVSMEntry(b), toXVSMEntry(c))
+      case (a: Serializable, b: Serializable, c: Serializable, d: Serializable) => new org.xvsm.core.Tuple(toXVSMEntry(a), toXVSMEntry(b), toXVSMEntry(c), toXVSMEntry(d))
+      case x: Serializable => new org.xvsm.core.AtomicEntry[Serializable](x, x.getClass.asInstanceOf[Class[Serializable]])
     }
   }
-  def fromXVSMEntry(entry: org.xvsm.core.Entry) = {
+  def fromXVSMEntry[T](entry: org.xvsm.core.Entry): T = {
     entry match {
-      case e: org.xvsm.core.VoidEntry => None
-      case e: org.xvsm.core.AtomicEntry[_] => e.getValue
-      case e: org.xvsm.core.Tuple if e.size == 2 => (e.getEntryAt(0), e.getEntryAt(1))
-      case e: org.xvsm.core.Tuple if e.size == 3 => (e.getEntryAt(0), e.getEntryAt(1), e.getEntryAt(2))
-      case e: org.xvsm.core.Tuple if e.size == 4 => (e.getEntryAt(0), e.getEntryAt(1), e.getEntryAt(2), e.getEntryAt(3))
+      case e: org.xvsm.core.VoidEntry => None.asInstanceOf[T]
+      case e: org.xvsm.core.AtomicEntry[T] => e.getValue
+      case e: org.xvsm.core.Tuple if e.size == 2 => (fromXVSMEntry[Serializable](e.getEntryAt(0)), fromXVSMEntry[Serializable](e.getEntryAt(1))).asInstanceOf[T]
+      case e: org.xvsm.core.Tuple if e.size == 3 => (fromXVSMEntry[Serializable](e.getEntryAt(0)), fromXVSMEntry[Serializable](e.getEntryAt(1)), fromXVSMEntry[Serializable](e.getEntryAt(2))).asInstanceOf[T]
+      case e: org.xvsm.core.Tuple if e.size == 4 => (fromXVSMEntry[Serializable](e.getEntryAt(0)), fromXVSMEntry[Serializable](e.getEntryAt(1)), fromXVSMEntry[Serializable](e.getEntryAt(2)), fromXVSMEntry[Serializable](e.getEntryAt(3))).asInstanceOf[T]
     }
   }
 }
@@ -46,10 +46,26 @@ class Container(val elevator: SpaceElevator, val backing: org.xvsm.core.Containe
   }
 
   def read[T](tx: Transaction, timeout: Int, selectors: org.xvsm.selectors.Selector*): Seq[T] = {
-    readRaw(tx, timeout, selectors: _*).map(Container.fromXVSMEntry(_)).asInstanceOf[Seq[T]]
+    readRaw(tx, timeout, selectors: _*).map(Container.fromXVSMEntry[T](_))
+  }
+
+  def readOne[T](tx: Transaction, timeout: Int, selectors: org.xvsm.selectors.Selector*): Option[T] = {
+    try {
+      read[T](tx, timeout, selectors: _*).firstOption
+    } catch {
+      case e: org.xvsm.internal.exceptions.CountNotMetException => None
+    }
   }
 
   def take[T](tx: Transaction, timeout: Int, selectors: org.xvsm.selectors.Selector*): Seq[T] = {
-    takeRaw(tx, timeout, selectors: _*).map(Container.fromXVSMEntry(_)).asInstanceOf[Seq[T]]
+    takeRaw(tx, timeout, selectors: _*).map(Container.fromXVSMEntry[T](_))
+  }
+
+   def takeOne[T](tx: Transaction, timeout: Int, selectors: org.xvsm.selectors.Selector*): Option[T] = {
+    try {
+      take[T](tx, timeout, selectors: _*).firstOption
+    } catch {
+      case e: org.xvsm.internal.exceptions.CountNotMetException => None
+    }
   }
 }
