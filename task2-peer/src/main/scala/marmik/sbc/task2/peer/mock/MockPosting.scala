ea5a53963e7a4cc5d04f1ae1e66dfd37b0d31613
Martin Schürrer <martin@schuerrer.org>
2009-01-10 19:27:58 +0100

    Fix deprecation

--- task2-peer/src/main/scala/marmik/sbc/task2/peer/mock/MockPosting.scala
+++ task2-peer/src/main/scala/marmik/sbc/task2/peer/mock/MockPosting.scala
@@ -7,7 +7,7 @@ import marmik.sbc.task2.peer._
 class MockPosting(val session: Session, val subject: String, val author: String, var content: String, val createdAt: GregorianCalendar, val topic: MockTopic, var parent: MockPosting, var replies: List[MockPosting]) extends Posting {
   def reply(author:String, subject:String, content:String):Posting = {
     val posting = new MockPosting(session, subject, author, content, null, topic, this, List())
-    replies += posting
+    replies = posting :: replies
     posting
   }
   def edit(newContent: String) {
