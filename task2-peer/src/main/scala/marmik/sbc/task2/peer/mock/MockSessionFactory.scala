6e1fde26eb0a1e58dd7da39d31491c437ac28d05
Martin Schürrer <martin@schuerrer.org>
2009-01-10 15:27:31 +0100

    Renamed MockSessionFactory

--- task2-peer/src/main/scala/marmik/sbc/task2/peer/mock/MockSessionFactory.scala
+++ task2-peer/src/main/scala/marmik/sbc/task2/peer/mock/MockSessionFactory.scala
@@ -3,6 +3,6 @@ package marmik.sbc.task2.peer.mock
 import marmik.sbc.task2.peer._
 
 class MockSessionFactory extends SessionFactory {
-  def name() = "XVSM (Mock)"
+  def name() = "Mock"
   def login(url: String, selfName: String): Session = new MockSession(url, selfName)
 }
